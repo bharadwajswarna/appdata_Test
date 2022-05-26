0a6eea304fd8bccca09e712c95bd2203d30b14cf
bharadwajswarna <95418286+bharadwajswarna@users.noreply.github.com>
2022-05-26 09:30:21 +0530

    Updated 3 apps

--- reviews_st.py
+++ reviews_st.py
@@ -17,7 +17,8 @@ add_selectbox = st.sidebar
 with add_selectbox:
     st.title("Aidetic App Analyzer")
     st.write('**Choose an App**')
-    app_choice = st.selectbox(label = "The reviews of the following apps from play store have been analyzed",options = ("HDFC PayZapp", "NPCI Bhim", "Zest Money"),help = "choose the app you want")
+    app_choice = st.selectbox(label = "The reviews of the following apps from play store have been analyzed",options = ("HDFC PayZapp", "NPCI Bhim", "Zest Money",
+                                        "PhonePe Business","GooglePay Business","Kredit Bee"),help = "choose the app you want")
     st.write('**Choose Themes**')
     theme_choice = st.radio(label = "Positive Themes represents ideas/themes people like about the app",options = ['Positive Themes','Negative Themes','All Themes'])
     #st.write("**Compare Reviews**")
@@ -41,6 +42,16 @@ with header:
     
     if app_choice == "Zest Money":
         v = {0:"4.4",1:"167 K",2:"5 M+"}
+
+    if app_choice == "GooglePay Business":
+        v = {0:"4.5",1:"190 K",2: "10 M+"}
+    
+    if app_choice == "PhonePe Business":
+        v = {0:"4.5",1:"307 K",2:"10 M+"}
+    
+    if app_choice == "Kredit Bee":
+        v = {0:"4.4",1:"780 K",2:"10 M+"}
+    
 
     col1, col2, col3 = st.columns(3)
     col1.metric(label="Rating out of 5", value=v[0])
@@ -66,25 +77,25 @@ with header:
 
 with test:
 
-    st.header("Themes")
+    st.title("Themes")
 
 # hdfc
     
     if (app_choice == "HDFC PayZapp") & (theme_choice == 'Negative Themes'):
 
-        st.title("Negative Themes")
+        st.header("Negative Themes")
         image = Image.open('hdfc_neg.png')
         st.image(image, caption='HDFC PayZapp Negative Reviews')
 
     elif (app_choice == "HDFC PayZapp") & (theme_choice == 'Positive Themes'):
 
-        st.title("Positive Themes")
+        st.header("Positive Themes")
         image = Image.open('hdfc_pos.png')
         st.image(image, caption='HDFC PayZapp Positive Reviews')
     
     elif (app_choice == "HDFC PayZapp") & (theme_choice == 'All Themes'):
 
-        st.title("All Themes")
+        st.header("All Themes")
         image = Image.open('hdfc_all.png')
         st.image(image, caption='HDFC PayZapp Reviews')
 
@@ -93,19 +104,19 @@ with test:
     
     elif (app_choice == "Zest Money") & (theme_choice == 'Negative Themes'):
 
-        st.title("Negative Themes")
+        st.header("Negative Themes")
         image = Image.open('zest_neg.png')
         st.image(image, caption='Zest Money Negative Reviews')
 
     elif (app_choice == "Zest Money") & (theme_choice == 'Positive Themes'):
 
-        st.title("Positive Themes")
+        st.header("Positive Themes")
         image = Image.open('zest_pos.png')
         st.image(image, caption='Zest Money Positive Reviews')
     
     elif (app_choice == "Zest Money") & (theme_choice == 'All Themes'):
 
-        st.title("All Themes")
+        st.header("All Themes")
         image = Image.open('zest_all.png')
         st.image(image, caption='Zest Money Reviews')
 
@@ -114,22 +125,82 @@ with test:
 
     elif (app_choice == "NPCI Bhim") & (theme_choice == 'Negative Themes'):
 
-        st.title("Negative Themes")
+        st.header("Negative Themes")
         image = Image.open('bhim_neg.png')
         st.image(image, caption='NPCI Bhim Negative Reviews')
 
     elif (app_choice == "NPCI Bhim") & (theme_choice == 'Positive Themes'):
 
-        st.title("Positive Themes")
+        st.header("Positive Themes")
         image = Image.open('bhim_pos.png')
         st.image(image, caption='NPCI Bhim Positive Reviews')
     
     elif (app_choice == "NPCI Bhim") & (theme_choice == 'All Themes'):
 
-        st.title("All Themes")
+        st.header("All Themes")
         image = Image.open('bhim_all.png')
         st.image(image, caption='NPCI Bhim Reviews')
+
+# google pay business
+
+    elif (app_choice == "GooglePay Business") & (theme_choice == 'Negative Themes'):
+
+        st.header("Negative Themes")
+        image = Image.open('googlepaybusiness_neg.png')
+        st.image(image, caption='GooglePay Business Negative Reviews')
+
+    elif (app_choice == "GooglePay Business") & (theme_choice == 'Positive Themes'):
+
+        st.header("Positive Themes")
+        image = Image.open('googlepaybusiness_pos.png')
+        st.image(image, caption='GooglePay Business Positive Reviews')
     
+    elif (app_choice == "GooglePay Business") & (theme_choice == 'All Themes'):
+
+        st.header("All Themes")
+        image = Image.open('googlepaybusiness_all.png')
+        st.image(image, caption='GooglePay Business Reviews')
+
+# phonepe business
+
+    elif (app_choice == "PhonePe Business") & (theme_choice == 'Negative Themes'):
+
+        st.header("Negative Themes")
+        image = Image.open('phonepebusiness_neg.png')
+        st.image(image, caption='PhonePe Business Negative Reviews')
+
+    elif (app_choice == "PhonePe Business") & (theme_choice == 'Positive Themes'):
+
+        st.header("Positive Themes")
+        image = Image.open('phonepebusiness_pos.png')
+        st.image(image, caption='PhonePe Business Positive Reviews')
+    
+    elif (app_choice == "PhonePe Business") & (theme_choice == 'All Themes'):
+
+        st.header("All Themes")
+        image = Image.open('phonepebusiness_all.png')
+        st.image(image, caption='PhonePe Business Reviews')
+
+# kredit bee
+
+    elif (app_choice == "Kredit Bee") & (theme_choice == 'Negative Themes'):
+
+        st.header("Negative Themes")
+        image = Image.open('kreditbee_neg.png')
+        st.image(image, caption='Kredit Bee Negative Reviews')
+
+    elif (app_choice == "Kredit Bee") & (theme_choice == 'Positive Themes'):
+
+        st.header("Positive Themes")
+        image = Image.open('kreditbee_pos.png')
+        st.image(image, caption='Kredit Bee Positive Reviews')
+    
+    elif (app_choice == "Kredit Bee") & (theme_choice == 'All Themes'):
+
+        st.header("All Themes")
+        image = Image.open('kreditbee_all.png')
+        st.image(image, caption='Kredit Bee Reviews')
+
 
     else:
 
